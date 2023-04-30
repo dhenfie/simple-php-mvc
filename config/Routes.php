48e4db9178a33f17165b132fbd8d307e64652312
dhenfie <fajarsusilo1600@gmail.com>
2023-04-30 19:58:32 +0700

    refactor: penambahan dokumentasi

--- config/Routes.php
+++ config/Routes.php
@@ -1,9 +1,13 @@
 <?php
 use System\Routing\Route;
 
+// set namespace controller
 Route::setNamespace('App\\Controllers\\');
+
+// register ErrorController untuk mengatur halaman 404
 Route::setError('ErrorController::error');
 
+// petakan route
 Route::add('/', 'HomeController::main');
 Route::add('auth/login', 'AuthController::login');
 Route::add('auth/logout', 'AuthController::logout');
